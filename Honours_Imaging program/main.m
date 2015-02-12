eec59335e26932ba505d84ee11e08302e7fe2cb2
kkroep <k.kroep1@gmail.com>
2015-02-12 10:28:26 +0100

    polijsterij

--- Honours_Imaging program/main.m
+++ Honours_Imaging program/main.m
@@ -8,10 +8,10 @@ disp('IMAGING SIMULATOR by Kees Kroep');
 
 S_Resolution = 1E-3; %distance between voxels
 S_Start = [0,0,0]; % the s  tartlocation of the sourcespace
-S_Directions = [48,48,48];
+S_Directions = [32,32,32];
 %V_Slices = [1,3,5,7]; % four slices out of the measured data
 %frequency = 50000; % hz, we currenlt0y only use one frequency
-frequency2 = [10000, 60000, 100000];
+frequency = [10000, 60000, 100000];
 new_Source_3D = zeros(S_Directions(1),S_Directions(2),S_Directions(3));
 
 c = 320; %speed of sound 
@@ -26,18 +26,17 @@ end
 if(~exist('old_Receiver_locs'))
     old_Receiver_locs=0;
 end
-if(~exist('old_frequency2'))
-    old_frequency2=[0,0,0];
+if(~exist('old_frequency'))
+    old_frequency=[0,0,0];
 end
 
-disp('Frequency_Loop STARTED');
 i=1;
-for frequency = frequency2
+for frequency_tmp = frequency
     
-fprintf('NEW LOOP Frequency =%d\n\n',frequency);
+fprintf('NEW LOOP Frequency = %d KHz\n\n',frequency_tmp/1000);
 %disp(old_S_Directions == S_Directions AND old_Receiver_locs == Receiver_locs AND old_frequency2(i) == frequency2(i));
-if(isequal(old_S_Directions,S_Directions) && isequal(old_Receiver_locs,Receiver_locs) && old_frequency2(i) == frequency2(i))
-    fprintf('\tA_Matrix from whos\tSTARTED\tDONE\tSPEED-UP\n');
+if(isequal(old_S_Directions,S_Directions) && isequal(old_Receiver_locs,Receiver_locs) && old_frequency(i) == frequency(i))
+    fprintf('\tA_Matrix from Workspace \tDONE\tSPEED-UP!\n');
     tic;
 else
 
@@ -45,7 +44,7 @@ fprintf('\tcalculate_A \t\tSTARTED');
 tic;
 %Calculate the A_matrix. This matrix is used for both the forward and
 %inverse calculations.
-[ A_Matrix{i} ] = calculate_A( Receiver_locs, S_Directions, S_Resolution, S_Start, c, frequency);
+[ A_Matrix{i} ] = calculate_A( Receiver_locs, S_Directions, S_Resolution, S_Start, c, frequency_tmp);
 
 fprintf('\tDONE\t');
 fprintf('%f seconds\n', toc);
@@ -89,39 +88,38 @@ i=i+1;
 end
 
 
-disp('Frequency_Loop DONE');
-
 %Weghalen van nutteloze variabelen uit de workspace voor een cleanere
 %representatie
 varlist1 = {'x','y','z', 'varlist1'};
 clear(varlist1{:}); 
-varlist2 = {'S_Resolution', 'frequency', 'c', 'S_Start', 'varlist2', 'i', ...
-                           'new_Source', 'old_Receiver_locs', 'old_S_Directions', 'old_frequency2', 'transmitter_string'};
+varlist2 = {'S_Resolution', 'frequency_tmp', 'c', 'S_Start', 'varlist2', 'i', ...
+                           'new_Source', 'old_Receiver_locs', 'old_S_Directions', 'old_frequency', 'transmitter_string'};
 clear(varlist2{:});
-
-
-
-
 
 disp('TODOLIST:');
 
 fprintf('-\treceivers op een bol gooien\n');
+fprintf('-\tGUI imaging op zn kop geplot\n');
+fprintf('-\tGUI start niet netjes\n');
 fprintf('-\truis op data (experiementeren)\n');
 fprintf('-\textra snelheidswinst?\n');
 
+
+
+whos; %View workspace
 
 %Make a cell-array of strings used for the GUI
 for i=1:size(Transmitter_locs,1)
 transmitter_string{i} = strcat('[',num2str(Transmitter_locs(i,1)), ',', num2str(Transmitter_locs(i,2)), ',', num2str(Transmitter_locs(i,3)),']');
 end
 
-whos; %View workspace
+
 
 
 %store old settings for possible SPEED-UP
 old_S_Directions = S_Directions;
 old_Receiver_locs = Receiver_locs;
-old_frequency2 = frequency2;
+old_frequency = frequency;
 
 %Launch GUI
 Imaging_GUI(real(new_Source_3D), S_Directions, transmitter_string);
